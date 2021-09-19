5a4c33ac3f690e196d943e36e7389e09cb5c867e
toasahi <asa1313to@gmail.com>
2021-09-19 17:18:03 +0900

    Add graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats.controller';
+import { DogController } from './dog/dog.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController,CatsController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      playground: true,
+      autoSchemaFile: 'schema.graphql'
+    }),
+    RecipesModule,
+  ],
 })
 export class AppModule {}
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController,CatsController, DogController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
